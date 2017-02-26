3f32a863e1b63ed6f0d356cd5ff398bbca2ebb41
HelenaSollie <helena.sollie@gmail.com>
2017-02-26 13:06:18 +0100

    Added task 3 PHP

---
+++ TAAK2_Advanced/contact.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>Pinterest: Contact</title>
+</head>
+
+<body>
+
+<?php include ("nav.inc.php")?>
+
+<h1>Contact</h1>
+<p>Dit is de contactpagina</p>
+
+<?php include ("footer.inc.php")?>
+
+
+</body>
+</html>
---
+++ TAAK2_Advanced/home.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>Pinterest: Home</title>
+</head>
+
+<body>
+
+    <?php include ("nav.inc.php")?>
+
+    <h1>Home</h1>
+    <p>Dit is de homepagina</p>
+
+    <?php include ("footer.inc.php")?>
+
+
+</body>
+</html>
